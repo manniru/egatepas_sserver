b46e9423911597bad0c5e4866adcd6c9d3d90671
Mannir <manniru@gmail.com>
2019-02-19 07:05:47 +0100

    Added Backend Server

---
+++ src/config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    db: 'mongodb://localhost/egatepass' //process.env.DB
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,18 +1,64 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import EGatePassApp from "./mannir/egatepass/EGatePassApp";
-/*
-import "./styles.css";
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const socket = require('socket.io');
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+const config = require('./config');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.db);
+let db = mongoose.connection;
+
+db.on('open', () => {
+  console.log('Connected to the database.');
+});
+
+db.on('error', (err) => {
+  console.log(`Database error: ${err}`);
+});
+
+const app = express();
+
+app.enable('trust proxy');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+if (process.env.CORS) {
+  app.use(cors());
 }
-*/
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<EGatePassApp />, rootElement);
+app.use('/api/users', require('./users'));
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) return next(err);
+  res.status(400).json({ err: err });
+});
+
+const port = process.env.PORT || 1313;
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+const io = socket(server);
+let online = 0;
+
+io.on('connection', (socket) => {
+  online++;
+  console.log(`Socket ${socket.id} connected.`);
+  console.log(`Online: ${online}`);
+  io.emit('visitor enters', online);
+
+  socket.on('add', data => socket.broadcast.emit('add', data));
+  socket.on('update', data => socket.broadcast.emit('update', data));
+  socket.on('delete', data => socket.broadcast.emit('delete', data));
+
+  socket.on('disconnect', () => {
+    online--;
+    console.log(`Socket ${socket.id} disconnected.`);
+    console.log(`Online: ${online}`);
+    io.emit('visitor exits', online);
+  });
+});
